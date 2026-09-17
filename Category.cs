abe73be956d21392441141aff036c244d49fdcb0
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2026-09-17 09:43:33 +0200

    Add project files.

---
+++ Category.cs
@@ -0,0 +1,26 @@
+﻿class Category
+{
+    private int survivals;
+    private string name;
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrEmpty(value)) throw new Exception("nem lehet üres!");
+            name = value;
+        }
+    }
+    public int Survivals
+    {
+        get => survivals;
+        set
+        {
+            if (value < 0 || value > 1000) throw new Exception("hülye vagy bazdmeg!");
+
+            survivals = value;
+        }
+    }
+    public int Missing { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Category cat = new()
+{
+    Name = "",
+    Survivals = 200,
+    Missing = 50
+};
+
+Console.WriteLine(cat is not null  ? "oké" : "nem oké");
